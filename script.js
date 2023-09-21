36fc26d987cf11591aed05f0f9c71055fff0173f
temesvarilaszlo <113339488+temesvarilaszlo@users.noreply.github.com>
2023-09-21 09:47:42 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,129 +1,172 @@
-console.log("Hello world!");
-// JS doksi: https://developer.mozilla.org/en-US/docs/Web/JavaScript
-// GitHub: https://github.com/Valentinusz/webprog-2023-24-1-5
-// Canvas: https://canvas.elte.hu/courses/38953
+// DOM (Document Object Model)
+// egy fa szerkezetű dokumentum (pl. SVG, XML, *HTML*) modellezése objektumokkal
+// JS esetében általában a HTML DOM-ra gondolunk DOM alatt
+// a DOM kényelmes interfészt biztosít, a HTML dokumentum manipulálásához
+// objektumfa gyökere: document (globális változó)
+// fát Node objektumok alkotják
+// Node objektumnak 4 fajtája lehet
+// 1. Document
+// 2. Text (szöveges tartalom)
+// 3. Element (HTML elemek)
+// 4. Attribute (HTML elemek attribútumai)
+// CSS szelektorok ismétléséhez ajánlom a CSS Diner (https://flukeout.github.io/) oldalt
 
-// 2. Készítsd el a Fahrenheitből Celsius fokba átalakító függvényt! 5/9 * (F-32)
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
-function f2c(F)
-{
-    return 5/9 * (F-32);
-}
-console.log(f2c(32));
+console.log(document);
 
-const f2c2 = function(F){
-    return 5/9 * (F-32);
-}
+// 1. Egy gomb megnyomására írd ki a dokumentum valamelyik általad választott részére, hogy "Helló világ!"!
 
-const f2c3 = (F) => {
-    return 5/9 * (F-32);
-}
+const button = document.querySelector('button#greet');
+console.log(button);
 
-const f2c4 = F => (5/9 * (F-32));
+button.addEventListener('click', () => {
+    button.insertAdjacentText('beforeend', 'Hello world!');
+    // button.innerHTML += '<a>Hello world</a>'
+    // button.innerText += '<a>Hello world</a>'
+});
 
-// 7. Adott két szám. Írj függvényt, amely visszaadja legnagyobb közös osztójukat!
-function lnko(a, b)
-{
-    if (a < b)
+// 2. Kérj be egy számot, és annyiszor írd ki egymás alá egyre növekvő betűméretekkel a "Hello világ!" szöveget!
+
+const printButton = document.querySelector('button#print-button');
+const countInput = document.querySelector('input#print-count');
+const secondTask = document.querySelector('article:nth-of-type(2)')
+
+console.log(printButton, countInput);
+
+printButton.addEventListener('click', () => {
+    const printCount = Number.parseInt(countInput.value, 10); 
+
+    for (let i = 0; i < printCount; i++)
     {
-        [a,b] = [b,a]; //csere
+        const element = document.createElement('h3');
+        element.innerText = 'Hello world';
+        element.style.fontSize = `${1 + 0.25 * i}em`;
+
+        // utolsó gyerekként szúrja be
+        secondTask.appendChild(element);
     }
-    let maradek = a % b;
-    while (maradek > 0)
+    console.log(printCount);
+});
+
+// 3. Kérj be egy N számot, és készíts azzal egy NxN-es szorzótáblát!
+
+const size = 10;
+
+const thirdTask = document.querySelector('article:nth-of-type(3)');
+console.log(thirdTask);
+
+const table = document.createElement('table');
+
+for (let i = 1; i < size; i++)
+{
+    const row = document.createElement('tr')
+    for (let j = 1; j < size; j++)
     {
-        a = b;
-        b = maradek;
-        maradek = a % b;
+        const cell = document.createElement('td');
+        cell.innerText = i * j;
+        row.appendChild(cell);
     }
-    return b;
+    table.appendChild(row);
 }
+thirdTask.appendChild(table);
+// 6. Adott két szöveges beviteli mező és köztük egy gomb.
+// A gomb lenyomására másold át az egyik szöveges beviteli mező tartalmát a másikba!
 
-console.log(lnko(30,20));
+const input1 = document.querySelector('article#task-6 label input');
+const input2 = document.querySelector('article#task-6 label:nth-of-type(2) input');
+const copyButton = document.querySelector('article#task-6 button');
 
-// tömbfüggvények
+copyButton.addEventListener('click', () => { input2.value = input1.value; })
 
-// 11. Egy számsorozatban keress meg egy negatív számot.
-const numbers = [2, 5, 9, -1, 3, 5, 1, 2, 1];
+// 7. Egy űrlapon csak akkor kérd be a leánykori nevet, ha nő az illető! Használd a rádiógombok click eseményét!
+// A megjelenítéshez, eltűntetéshez használd az elemek hidden tulajdonságát!
 
-console.log(numbers.find(number => number < 0));
-console.log(numbers.findIndex(number => number < 0));
+// 8. Oldalbetöltéskor listázd ki az oldal összes hiperhivatkozásának a címét!
 
-// 12. Számold meg, hány páros szám van egy számokat tartalmazó tömbben!
+const links = document.querySelectorAll('a');
+links.forEach(({href}) => console.log(href));
+// 10. Készíts egy számlálót komponenst!
+const task10 = document.querySelector('article#task10');
+const minus = task10.querySelector('#down');
+const plus = task10.querySelector('#up');
+const numInput = task10.querySelector('#num');
 
-console.log("Páros számok száma: ", numbers.filter(number => number % 2 === 0).length);
+minus.addEventListener('click', () => numInput.value++);
+plus.addEventListener('click', () => numInput.value--);
 
-// 13. Válogasd ki azokat a számokat, amelyek mindkét szomszédjuktól egy előre bekért értéken belül térnek el.
-const limit = 2;
-console.log(numbers.filter((currentValue, currentIndex, array) => {
-    return Math.abs(currentValue - array[currentIndex - 1]) < limit && Math.abs(currentValue - array[currentIndex - 1]) < limit;
-}))
+// 12. Készíts webes alkalmazást kamatos kamat számolására.
+// A számoláshoz meg kell adni a kiindulási összeget, a kamat értékét, valamint azt, hány évvel későbbi összegre vagyunk
+// kíváncsiak. Minden év végén adjuk hozzá a kamatot a tőkéhez, és a következő évben az képezi a kamatozás alapját.
+// A feladat során jelenítsük meg azt is, hogy melyik évben hogyan változik az összeg.
 
-// 16. Döntsd el egy mátrixról, hogy minden eleme páros-e!
-const matrix = [[1, 2, 3], [2, 3, 4], [6, 4, 2]];
+const task12 = document.querySelector('article#task12');
+const moneyInput = task12.querySelector('#money');
+const interestInput = task12.querySelector('#interest');
+const yearInput = task12.querySelector('#year');
+const calculateButton = task12.querySelector('#calculate');
+const moneyTableBody = task12.querySelector('#breakdown').tBodies[0];
 
-console.log(matrix.every(row => row.every(number => number % 2 === 0)))
-console.log(matrix.every(row => row.some(number => number % 2 === 0)))
+calculateButton.addEventListener('click', () => {
+    let money = Number.parseInt(moneyInput.value);
+    const interest = Number.parseFloat(interestInput.value, 10);
+    const year = Number.parseFloat(yearInput.value, 10);
 
+    if (money && interest && year){
+        moneyTableBody.innerText = "";
 
-// Objects
-const obj = {
-    mezo1 : 12,
-    'mezo2': '12',
-    metodus(){
-        console.log(this.mezo1, this.mezo2);
+        for (let i = 0; i < year; i++){
+            const row = document.createElement('tr');
+            row.innerHTML = `<td>${i}</td><td>${money}</td>`
+            money *= (1 + (interest / 100));
+
+            moneyTableBody.appendChild(row);
+        }
+    }
+    else{
+        console.error('Hibás bemeneti értékek!');
+    }
+    console.log(money, interest, year);
+})
+
+// 14. Adott egy könyvtári nyilvántartás. Egy könyvről a következő adatokat tároljuk:
+// szerző cím kiadás éve kiadó ISBN szám
+// a. Felületen kérj be egy évszámot, és listázd ki az abban az évben megjelent könyvcímeket!
+// b. Készíts egy legördülő mezőt, amelyben az egyes kiadók vannak felsorolva. Egy gombra kattintva táblázatos formában
+// jelenítsd meg a kiválasztott kiadóhoz tartozó könyveket!
+
+const books = [
+    {
+        szerzo: "J.K. Rowling",
+        cim: "Harry Potter és a Bölcsek Köve",
+        kiadasEve: 1997,
+        kiado: "Bloomsbury",
+        ISBN: "978-963-8386-65-4"
     },
-    metodus2: function(){
-        console.log(this.mezo1, this.mezo2);
+    {
+        szerzo: "George Orwell",
+        cim: "1984",
+        kiadasEve: 1949,
+        kiado: "Secker and Warburg",
+        ISBN: "978-0451524935"
+    },
+    {
+        szerzo: "Harper Lee",
+        cim: "Ne bántsátok a feketerigót!",
+        kiadasEve: 1960,
+        kiado: "J. B. Lippincott & Co.",
+        ISBN: "978-0061120084"
+    },
+    {
+        szerzo: "Paulo Coelho",
+        cim: "The Fifth Mountain",
+        kiadasEve: 1997,
+        kiado: "HarperOne",
+        ISBN: "978-0061723731"
+    },
+    {
+        szerzo: "J.K. Rowling",
+        cim: "Harry Potter and the Chamber of Secrets",
+        kiadasEve: 1998,
+        kiado: "Bloomsbury",
+        ISBN: "978-0-7475-3849-0"
     }
-}
-obj.metodus();
-obj.metodus2();
-
-
-class Movie{
-    constructor(title, length, category, directors){
-        this.title = title;
-        this.length = length;
-        this.category = category;
-        this.directors = [...directors];
-    }
-}
-// 27. a, b, c
-const movies = [
-    new Movie("Harry Potter and the Philosopher's Stone", 152, "fantasy", ["David Heyman"]),
-    new Movie("Harry Potter and the Prisoner of Azkaban", 142, "fantasy", ["David Heyman", "Chris Columbus", "Mark Radcliffe"]),
-    new Movie("Harry Potter and the Half-Blood Prince", 153, "fantasy", ["David Heyman", "David Barron"])
-]
-console.log(movies);
-
-// a
-
-movies.forEach(movie => console.log(movie));
-movies.forEach(console.log);
-console.table(movies);
-
-// b
-
-console.table(movies.filter(({directors}) => directors.length > 1));
-
-// c 
-
-console.log(movies.reduce((longest, currentMovie) => currentMovie.length > longest.length ? currentMovie : longest), movies[0])
-
-// 29. a, b, c, d, e, g, h 
-const temperatures = [5, 6, -1, 8, 12, 13, 15, 4, -2, 5, 9, 15, 20, 19, 17];
-
-console.log(temperatures.filter(temp => temp <= 0));
-
-console.log(temperatures.map(temp => temp + '°C'));
-
-console.log(temperatures.reduce((max, currtemp) => (currtemp > max ? currtemp : max), -Infinity));
-
-console.log(temperatures.filter(temp => temp < 20));
-
-console.log(temperatures.some(temp => temp > 40));
-
-console.log(temperatures.every(temp => temp > 0));
-
-console.log(temperatures.find(temp => temp > 10));
-// 28. házi
+];
